fa88bf623a0490c8ef8e5ce02ae456ea5a85e5f0
Krzysztof <k.kozlowski.tychy@gmail.com>
2018-10-31 17:04:34 +0100

    Created HomeBudget class

---
+++ HomeBudget.h
@@ -0,0 +1,29 @@
+#ifndef HOMEBUDGET_H
+#define HOMEBUDGET_H
+
+#include <iostream>
+
+#include "UserManager.h"
+//#include "IncomeManager.h"
+//#include "ExpenseManager.h"
+
+using namespace std;
+
+class HomeBudget {
+    UserManager userManager;
+    //IncomeManager *incomeManager;
+    //ExpenseManager *expenseManager;
+    const string fileNameWithIncomes;
+    const string fileNameWithExpenses;
+
+public:
+    HomeBudget(string fileNameWithUsers, string FILENAMEWITHINCOMES, string FILENAMEWITHEXPENSES) :
+        userManager(fileNameWithUsers), fileNameWithIncomes(FILENAMEWITHINCOMES), fileNameWithExpenses(FILENAMEWITHEXPENSES){};
+    void userRegistration();
+    void userLogin();
+    void passwordChangeLoggedUser();
+    int getIdLoggedUser();
+    int userLogout();
+};
+
+#endif // HOMEBUDGET_H
