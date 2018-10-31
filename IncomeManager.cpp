c9694fafb455c6669a2c76d49b36900bc83c3389
Krzysztof <k.kozlowski.tychy@gmail.com>
2018-10-31 19:52:15 +0100

    Created IncomeManager class

---
+++ IncomeManager.cpp
@@ -0,0 +1,31 @@
+#include "IncomeManager.h"
+
+void IncomeManager::addIncome(){
+;
+}
+
+void IncomeManager::enterDataOfNewIncome(){
+;
+}
+
+void IncomeManager::loadIncomesLoggedUserFromFile(){
+;
+}
+
+double IncomeManager::calculateSumOfIncomes(){
+;
+}
+
+void IncomeManager::showIncomesList(){
+;
+}
+
+int IncomeManager::getStartBorderPeriod(){
+;
+}
+
+int IncomeManager::getEndBorderPeriod(){
+;
+}
+
+
